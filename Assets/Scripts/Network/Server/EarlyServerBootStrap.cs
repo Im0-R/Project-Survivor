ca9acbf27501134d5f6d143f18ada722074e468d
Léo-Paul VRAY <leopaulvray20@gmail.com>
2025-11-17 11:20:24 +0100

    Add EarlyServerBootStrap for pre-scene server start

---
+++ Assets/Scripts/Network/Server/EarlyServerBootStrap.cs
@@ -0,0 +1,25 @@
+#if UNITY_SERVER
+using UnityEngine;
+using Mirror;
+
+public static class EarlyServerBootstrap
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void Init()
+    {
+        Debug.Log("[EarlyServerBootstrap] Running BEFORE any scene loads...");
+
+        var nm = Object.FindObjectOfType<NetworkManager>();
+
+        if (nm != null && !NetworkServer.active)
+        {
+            Debug.Log("[EarlyServerBootstrap] Forcing Mirror StartServer()");
+            nm.StartServer();
+        }
+        else if (nm == null)
+        {
+            Debug.LogError("[EarlyServerBootstrap] No NetworkManager found!");
+        }
+    }
+}
+#endif
