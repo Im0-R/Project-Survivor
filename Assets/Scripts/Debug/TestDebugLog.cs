8b67056c610ad4e743d0f75a239f4bb7cc8a69dd
Im0-R <leopaulvray20@gmail.com>
2025-12-07 16:51:05 +0100

    Add debug logs for UNITY_CLIENT and UNITY_SERVER defines
    Added conditional debug logging in TestDebugLog.cs to indicate whether UNITY_CLIENT and UNITY_SERVER defines are set. Also updated .csproj files to synchronize DefineConstants for Unity version and platform consistency. Added meta files for SimpleNaturePack unitypackages.

--- Assets/Scripts/Debug/TestDebugLog.cs
+++ Assets/Scripts/Debug/TestDebugLog.cs
@@ -14,5 +14,16 @@ public class TestDebugLog : MonoBehaviour
         Debug.Log("Build type: " + (Application.isEditor ? "Editor" : "Build"));
 
         Debug.Log("[SERVER] Loaded Scene = " + SceneManager.GetActiveScene().name);
+
+#if !UNITY_CLIENT
+        Debug.Log("[SERVER] UNITY_CLIENT is NOT defined");
+#endif
+#if UNITY_SERVER
+        Debug.Log("[SERVER] UNITY_SERVER is defined");
+#endif
+#if UNITY_CLIENT
+        Debug.Log("[CLIENT] UNITY_CLIENT is defined");
+#endif
+
     }
 }
